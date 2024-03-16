fdcb0bf1570d8fc878281e79a4f3cf8a9bcdfa5d
RupaliMasal <144811962+RupaliMasal@users.noreply.github.com>
2024-03-16 16:12:39 +0530

    Adding SpecialOfferBg.js file

---
+++ multikart_ecommerce_project/src/App/Page/HomePage/App.js
@@ -0,0 +1,14 @@
+
+
+import SpecialOfferBg from './App/Page/HomePage/SpecialOfferBg';
+
+function App() {
+  return (
+    <div>
+      
+    <SpecialOfferBg/>
+    </div>
+  );
+}
+
+export default App;
---
+++ multikart_ecommerce_project/src/App/Page/HomePage/SpecialOfferBg.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../../../Assets/CSS/SpecialOffer.css'
+const SpecialOfferBg = () => {
+  return (
+    <div>
+      <div className="container-fluid s_bg-img">
+        <div className="row">
+            <div className="col-lg">
+            <div class=" col-lg-6 text-center">
+            <h2 className='s_banner1'>2023</h2>
+                <h3 className='s_banner2'>fashion trends</h3>
+                <h4 className='s_banner3'>special offer</h4>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SpecialOfferBg
