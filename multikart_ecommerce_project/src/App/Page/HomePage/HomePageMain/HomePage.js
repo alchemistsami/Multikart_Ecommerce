10c44d23398e507a0f73c3c975e902da58b384b2
RupaliMasal <rupalimasal2020@gmail.com>
2024-03-24 18:29:22 +0530

    dynamic background image

--- multikart_ecommerce_project/src/App/Page/HomePage/HomePageMain/HomePage.js
+++ multikart_ecommerce_project/src/App/Page/HomePage/HomePageMain/HomePage.js
@@ -3,6 +3,8 @@ import CarouselComponent from '../Carousel'
 import Poster from '../Poster'
 import { slide } from './data_HomePage'
 import { poster } from './data_HomePage'
+import SpecialOfferBg from '../SpecialOfferBg'
+import { bgimg } from './data_HomePage'
 function HomePage() {
    
   return (
@@ -10,6 +12,7 @@ function HomePage() {
         <div className="App">
           <CarouselComponent slide={slide} />
           <Poster poster={poster} />
+          <SpecialOfferBg  bgimg={bgimg}/>
         </div>
       </div>
   )
--- multikart_ecommerce_project/src/App/Page/HomePage/SpecialOfferBg.js
+++ multikart_ecommerce_project/src/App/Page/HomePage/SpecialOfferBg.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import '../../../Assets/CSS/SpecialOffer.css'
-const SpecialOfferBg = () => {
+import React from "react";
+import "../../../Assets/CSS/SpecialOffer.css";
+
+const SpecialOfferBg = ({ bgimg }) => {
   return (
     <div>
-      <div className="container-fluid s_bg-img">
-        <div className="row">
-            <div className="col-lg">
-            <div class=" col-lg-6 text-center">
-            <h2 className='s_banner1'>2023</h2>
-                <h3 className='s_banner2'>fashion trends</h3>
-                <h4 className='s_banner3'>special offer</h4>
-                </div>
-            </div>
-        </div>
-      </div>
+      {bgimg.map((bgimg)=>(
+           <div
+           className="container-fluid s_bg-img"
+           style={{ backgroundImage: `url(${bgimg.img})` }}
+         >
+           <div className="row">
+             <div className="col-lg">
+               <div className=" col-lg-6 text-center">
+                 <h2 className="s_banner1">{bgimg.bg1}</h2>
+                 <h3 className="s_banner2">{bgimg.bg2}</h3>
+                 <h4 className="s_banner3">{bgimg.bg3}</h4>
+               </div>
+             </div>
+           </div>
+         </div>
+      ))}
+     
     </div>
-  )
-}
+  );
+};
 
-export default SpecialOfferBg
+export default SpecialOfferBg;
